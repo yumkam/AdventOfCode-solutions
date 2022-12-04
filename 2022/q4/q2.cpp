2c5d0a68ab3ea997606b3cd212727e688f4c3506
Yuriy M. Kaminskiy <yumkam@gmail.com>
2022-12-04 05:09:07 +0000

    snapshot

---
+++ 2022/q4/q2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int ac, const char *av[]) {
+	//if (ac > 1 && av[1][0])
+	//	maxs = atoi(av[1]);
+	int ret = 0;
+	int a, b, c, d;
+	char ch;
+	while(cin >>a>>ch>>b>>ch>>c>>ch>>d) {
+	    ret += a <= c && c <= b || c <= a && a <= d;
+	}
+	cout << ret << endl;
+	return 0;
+}
