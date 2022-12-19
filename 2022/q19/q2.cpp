526a5983c0c8a1c930b25035728a5f42bc540004
Yuriy M. Kaminskiy <yumkam@gmail.com>
2022-12-19 23:35:56 +0000

    snapshot

--- 2022/q19/q2.cpp
+++ 2022/q19/q2.cpp
@@ -5,18 +5,19 @@
 
 using namespace std;
 int main(int ac, const char *av[]) {
-    // robot type: [ore, clay, obsidan, geode]
-    // resources: [ore, clay, obsidan]
+    // robot type: [ore, clay, obsidian, geode]
+    // resources: [ore, clay, obsidian]
     array<array<unsigned,3>,4> costs {{0}};
     string s;
     unsigned i;
     char c;
-    unsigned ret = 1;
+    uint64_t ret = 1;
     const unsigned maxday = 32;
-    map<tuple<uint8_t, uint8_t, uint8_t>, array<uint8_t, 4>> m;
+    const unsigned maxprint = 3;
+    map<tuple<uint16_t, uint16_t, uint16_t>, array<uint16_t, 4>> m;
     while(cin >> s >> i >> c) { // <Blueprint> $num <:>
-	int blueprint = i;
-	if (blueprint > 3) break;
+	auto blueprint = i;
+	if (blueprint > maxprint) break;
 	// <Each> <XXX> <robot> <costs> $num <ore>
 	cin >> s >> s >> s >> s >>  i >> s;
 	array<unsigned,3> limits;
@@ -25,11 +26,11 @@ int main(int ac, const char *av[]) {
 	// <Each> <XXX> <robot> <costs> $num <ore>
 	cin >> s >> s >> s >> s >> i >> s;
 	costs[1][0] = i;
-	limits[0] += i;
+	limits[0] = max(i, limits[0]);
 	// <Each> <XXX> <robot> <costs> $num <ore>
 	cin >> s >> s >> s >> s >> i >> s;
 	costs[2][0] = i;
-	limits[0] += i;
+	limits[0] = max(i, limits[0]);
 	// <Each> <XXX> <robot> <costs> $num <ore>
 	// <and> $num <clay>
 	cin >> s >> i >> s;
@@ -38,9 +39,9 @@ int main(int ac, const char *av[]) {
 	// <Each> <XXX> <robot> <costs> $num <ore>
 	cin >> s >> s >> s >> s >> i >> s;
 	costs[3][0] = i;
-	limits[0] += i;
+	limits[0] = max(i, limits[0]);
 	// <Each> <XXX> <robot> <costs> $num <ore>
-	// <and> $num <obsidan>
+	// <and> $num <obsidian>
 	cin >> s >> i >> s;
 	costs[3][2] = i;
 	limits[2] = i;
@@ -50,12 +51,11 @@ int main(int ac, const char *av[]) {
 		clog << c << ' ';
 	    clog << endl;
 	}
-	// state: { collected: ore, clay, obsidan, geode; robots: ore, clay, obsidan, geode }
-	deque<array<array<uint8_t,4>, 2>> state, next;
+	// state: { collected: ore, clay, obsidian, geode; robots: ore, clay, obsidian, geode }
+	deque<array<array<uint16_t,4>, 2>> state, next;
 
 	state.emplace_back();
 	state.front()[1][0] = 1;
-	array<array<uint8_t,4>, 2> maxv {{0}};
 	for (unsigned day = 1; day <= maxday; day++) {
 	    next.clear();
 	    //next.reserve(state.size()*2);
@@ -73,12 +73,8 @@ int main(int ac, const char *av[]) {
 		s[0][1] += s[1][1];
 		s[0][2] += s[1][2];
 		s[0][3] += s[1][3];
-		for (unsigned i = 0; i < 3; i++)
-		    if (1 || save[i] <= limits[i]) {
 			next.push_back(s);
 			ins++;
-			break;
-		    }
 
 		//clog << '>'  << next.back()[0][0] << ' ' << next.back()[0][1] << ' ' << next.back()[0][2] << ' ' << next.back()[0][3] << '@'
 		//    << next.back()[1][0] << ' ' << next.back()[1][1] << ' ' << next.back()[1][2] << ' ' << next.back()[1][3] << ' ' << endl;
@@ -89,17 +85,8 @@ int main(int ac, const char *av[]) {
 			t[0][1] -= costs[type][1];
 			t[0][2] -= costs[type][2];
 			t[1][type]++;
-			unsigned lecnt = 0;
-			for (unsigned a = 0; a < 2; a++) 
-			    for (unsigned b = 0; b < 4; b++)
-				if (t[a][b] > maxv[a][b])
-				    maxv[a][b] = t[a][b];
-				else
-				    lecnt++;
-			if (lecnt != 2*4) {
 			    next.push_back(t);
 			    ins++;
-			}
 			//clog << '>'  << next.back()[0][0] << ' ' << next.back()[0][1] << ' ' << next.back()[0][2] << ' ' << next.back()[0][3] << '@'
 			//    << next.back()[1][0] << ' ' << next.back()[1][1] << ' ' << next.back()[1][2] << ' ' << next.back()[1][3] << ' ' << endl;
 		    }
@@ -109,6 +96,47 @@ int main(int ac, const char *av[]) {
 	    sort(next.begin(), next.end());
 	    next.erase(unique(next.begin(), next.end()), next.end());
 	    clog << ' ' << ins - next.size() << ' ' << next.size() << ' ' << next.size()*(1.0*sizeof(next[0])/1024/1024)<< endl;
+	    auto minmaxg = 0;
+	    for (auto &s: next) {
+		auto t = s;
+		// pessimistic scenario: we produce same amount of obsidian every day
+		// and build new geode robot once we collected enough obsidian and ore
+		for (unsigned nday = day + 1; nday <= maxday; nday++) {
+		    t[0][3] += t[1][3];
+		    if (t[0][2] >= costs[3][2]  && t[0][0] >= costs[3][0]) {
+		    	t[0][2] -= costs[3][2];
+		    	t[0][0] -= costs[3][0];
+		    	t[1][3]++;
+		    }
+		    t[0][2] += t[1][2];
+		    //t[1][2]++;
+		    t[0][0] += t[1][0];
+		    //t[1][0]++;
+		}
+		minmaxg = max<unsigned>(minmaxg, t[0][3]);
+	    }
+	    auto end = next.begin();
+	    for (auto &s: next) {
+		auto t = s;
+		// (overly) optimistic scenario: we build new obsidian and ore robot every day
+		// and (impossibly) at the same time build new geode robot once we collected obsidian
+		for (unsigned nday = day + 1; nday <= maxday; nday++) {
+		    t[0][3] += t[1][3];
+		    if (t[0][2] >= costs[3][2] && t[0][0] >= costs[3][0]) {
+			t[0][2] -= costs[3][2];
+			t[0][0] -= costs[3][0];
+			t[1][3]++;
+		    }
+		    t[0][2] += t[1][2];
+		    t[1][2]++;
+		    t[0][0] += t[1][0];
+		    t[1][0]++;
+		}
+		if (t[0][3] >= minmaxg)
+		    *end++ = s;
+	    }
+	    //clog << next.end() - end << endl;
+	    next.erase(end, next.end());
 	    state.swap(next);
 	}
 	unsigned maxg = 0;
