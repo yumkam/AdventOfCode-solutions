96319179808e9ced4d69c62353fd5ceb5de3119f
Yuriy M. Kaminskiy <yumkam@gmail.com>
2023-12-09 05:25:00 +0000

    snapshot

---
+++ 2023/q9/q2.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -aln
+use warnings;
+use strict;
+our $res;
+my @aoa = [@F];
+for (;;) {
+    my $anynz = 0;
+    my @a;
+    my $last = $aoa[$#aoa];
+    push @aoa, [];
+    my $cur = $aoa[$#aoa];
+    for my $i (1..$#{$last}) {
+	my $diff = $last->[$i] - $last->[$i-1];
+	$anynz ||= $diff;
+	push @$cur, $diff
+    }
+    last unless $anynz;
+}
+for (my $i = scalar @aoa; $i-- > 1; ) {
+    unshift @{$aoa[$i - 1]}, $aoa[$i - 1]->[0] - $aoa[$i]->[0];
+}
+warn $aoa[0]->[0];
+$res +=  $aoa[0]->[0];
+END {
+    print $res
+}
