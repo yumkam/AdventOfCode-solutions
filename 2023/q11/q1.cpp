463d45b165d70387db3ecdb5d9358939c0ce343e
Yuriy M. Kaminskiy <yumkam@gmail.com>
2023-12-11 05:48:15 +0000

    2023/q11: add q1

---
+++ 2023/q11/q1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    //vector<pair<unsigned, unsigned>> gal;
+    //vector<string> grid;
+    map<unsigned,vector<unsigned>> gals; // column->galaxy rows
+    for(unsigned row = 0; cin >> s; ++row) {
+	//grid.emplace_back(s);
+	size_t cnt = 0;
+	clog << '@' << row;
+	for (size_t pos = 0;; ) {
+	    auto next = s.find('#', pos);
+	    if (next == string::npos)
+		break;
+	    clog << ' ' << next;
+	    gals[next].push_back(row);
+	    ++cnt;
+	    pos = next + 1;
+	}
+	if (cnt == 0) {
+	    ++row; // double empty rows
+	    clog << 'E' << row;
+	}
+	clog << endl;
+    }
+    vector<pair<int, int>> garr;
+    unsigned prev = -1;
+    unsigned add = 0;
+    for (auto &[col,rows]: gals) {
+	add += col - prev - 1;
+	cout << col << '+' << add << endl;
+	for (auto r: rows)
+	    garr.emplace_back(r, col + add);
+	prev = col;
+    }
+    for (auto [i, j]: garr) clog << i <<','<<j<<' '; clog << endl;
+    uint64_t res = 0;
+    for (unsigned i = 0; i < garr.size(); ++i) {
+	auto [ir,ic] = garr[i];
+	for (unsigned j = 0; j < i; ++j) {
+	    auto [jr,jc] = garr[j];
+	    res += abs(jr - ir) + abs(ic - jc);
+	}
+    }
+    cout << res << endl;
+    //const auto n = grid.back().size();
+    //const auto m = grid.size();
+    return 0;
+}
