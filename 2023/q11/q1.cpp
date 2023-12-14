0b019953d28970dc0bc83f5268c10af7fff58390
Yuriy M. Kaminskiy <yumkam@gmail.com>
2023-12-14 17:37:48 +0300

    2023/q11/q1.cpp: disable debug messages

--- 2023/q11/q1.cpp
+++ 2023/q11/q1.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define debug(...)
+//#define debug(...) __VA_ARGS__
 int main() {
     string s;
     //vector<pair<unsigned, unsigned>> gal;
@@ -8,33 +10,33 @@ int main() {
     for(unsigned row = 0; cin >> s; ++row) {
 	//grid.emplace_back(s);
 	size_t cnt = 0;
-	clog << '@' << row;
+	debug(clog << '@' << row);
 	for (size_t pos = 0;; ) {
 	    auto next = s.find('#', pos);
 	    if (next == string::npos)
 		break;
-	    clog << ' ' << next;
+	    debug(clog << ' ' << next);
 	    gals[next].push_back(row);
 	    ++cnt;
 	    pos = next + 1;
 	}
 	if (cnt == 0) {
 	    ++row; // double empty rows
-	    clog << 'E' << row;
+	    debug(clog << 'E' << row);
 	}
-	clog << endl;
+	debug(clog << endl);
     }
     vector<pair<int, int>> garr;
     unsigned prev = -1;
     unsigned add = 0;
     for (auto &[col,rows]: gals) {
 	add += col - prev - 1;
-	cout << col << '+' << add << endl;
+	debug(clog << col << '+' << add << endl);
 	for (auto r: rows)
 	    garr.emplace_back(r, col + add);
 	prev = col;
     }
-    for (auto [i, j]: garr) clog << i <<','<<j<<' '; clog << endl;
+    debug(for (auto [i, j]: garr) clog << i <<','<<j<<' '; clog << endl);
     uint64_t res = 0;
     for (unsigned i = 0; i < garr.size(); ++i) {
 	auto [ir,ic] = garr[i];
